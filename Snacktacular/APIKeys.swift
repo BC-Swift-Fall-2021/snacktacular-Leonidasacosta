5789a7933b2244d9cda7677d396a5b42af7ba526
Leonidasacosta <89566738+Leonidasacosta@users.noreply.github.com>
2021-10-31 17:17:28 -0400

    Completed Video 8.7! Add Google Place Autocomplete!

---
+++ Snacktacular/APIKeys.swift
@@ -0,0 +1,12 @@
+//
+//  APIKeys.swift
+//  Snacktacular
+//
+//  Created by Leonidas Acosta on 10/31/21.
+//
+
+import Foundation
+
+struct APIKeys {
+    static let googlePlacesKey = "AIzaSyAEDTQs4ZebnzypzORPjdeQk2qwXugv4NE"
+}
