a73ee15219f4da52082b6994a46ff90e1e8a0b5c
Leonidasacosta <89566738+Leonidasacosta@users.noreply.github.com>
2021-11-04 00:18:05 -0400

    Completed Video 8.13! Creating the Review User Interface - Table View Controller with Static Cells!

--- Snacktacular/SpotDetailViewController.swift
+++ Snacktacular/SpotDetailViewController.swift
@@ -25,6 +25,13 @@ class SpotDetailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //hide keyboard if we tap outside of a field
+        let tap = UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing(_:)))
+        tap.cancelsTouchesInView = false
+        self.view.addGestureRecognizer(tap)
+        
+        
         tableView.delegate = self
         tableView.dataSource = self
         
@@ -88,6 +95,10 @@ class SpotDetailViewController: UIViewController {
         present(autocompleteController, animated: true, completion: nil)
     }
     
+    @IBAction func ratingButtonPressed(_ sender: UIButton) {
+        // TODO: eventually check if spot was saved. If it was not saved, save it & segue if save was succcessful. Otherwise, if it was saved successfully, segue as below:
+        performSegue(withIdentifier: "AddReview", sender: nil)
+    }
 }
 
 extension SpotDetailViewController: GMSAutocompleteViewControllerDelegate {
